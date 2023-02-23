4919cb949beb7e309219f72ea2826933dd55ee45
atsymuzaffer <a_muzafferr@hotmail.com>
2023-02-23 09:19:21 +0300

    Deneme

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+	public class CategoryManager:ICategoryService
+	{
+		EfCategoryRepository efCategoryRepository;
+
+		public CategoryManager()
+		{
+			efCategoryRepository = new EfCategoryRepository();
+		}
+		public void CategoryAdd(Category category)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CategoryDelete(Category category)
+		{
+			throw new NotImplementedException();
+
+		}
+
+		public void CategoryUpdate(Category category)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Category> GetList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Category GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
